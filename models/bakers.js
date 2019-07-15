e9883a8ad29f4216e9af38626643bec26e7309ea
hoaraucg@gmail.com <hoaraucg@gmail.com>
2019-07-15 11:57:20 -0400

    More Seeds playing

--- models/bakers.js
+++ models/bakers.js
@@ -1,62 +1,62 @@
-module.exports = function (sequelize, DataTypes) {
-    var Bakers = sequelize.define("Bakers", {
-        firstName: {
-            type: DataTypes.STRING,
-            allowNull: false,
-            validate: {
-                len: [1]
-            }
-        },
-        lastName: {
-            type: DataTypes.STRING,
-            allowNull: false,
-            validate: {
-                len: [1]
-            }
-        },
-        phoneNumber: {
-            type: DataTypes.STRING,
-            defaultValue: "1800-867-5309"
-        },
-        email: {
-            type: DataTypes.STRING,
-            allowNull: false,
-            validate: {
-                len: [1]
-            }
-        },
-        zipCode: {
-            type: DataTypes.INTEGER,
-            allowNull: false,
-            validate: {
-                len: [1]
-            }
-        },
-        streetAddress: {
-            type: DataTypes.STRING,
-            allowNull: false,
-            validate: {
-                len: [1]
-            }
-        },
-        city: {
-            type: DataTypes.STRING,
-            allowNull: false,
-            validate: {
-                len: [1]
-            }
-        },
-        state: {
-            type: DataTypes.STRING,
-            allowNull: false,
-            validate: {
-                len: [1]
-            }
-        },
-        image: {
-            type: DataTypes.STRING,
-            allowNull: true
-        }
-    });
-    return Bakers;
+module.exports = function(sequelize, DataTypes) {
+  var Bakers = sequelize.define("Bakers", {
+    firstName: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        len: [1]
+      }
+    },
+    lastName: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        len: [1]
+      }
+    },
+    phoneNumber: {
+      type: DataTypes.STRING,
+      defaultValue: "1800-867-5309"
+    },
+    email: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        len: [1]
+      }
+    },
+    zipCode: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      validate: {
+        len: [1]
+      }
+    },
+    streetAddress: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        len: [1]
+      }
+    },
+    city: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        len: [1]
+      }
+    },
+    state: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        len: [1]
+      }
+    },
+    image: {
+      type: DataTypes.STRING,
+      allowNull: true
+    }
+  });
+  return Bakers;
 };
