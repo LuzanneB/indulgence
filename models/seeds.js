2a5c7e168e6f464402ade1b89191d814bd232775
hoaraucg@gmail.com <hoaraucg@gmail.com>
2019-07-16 11:07:47 -0400

    Fixing Bakers table Seeding

--- models/seeds.js
+++ models/seeds.js
@@ -1,30 +1,39 @@
 var faker = require("faker");
 
-function generateBakers() {
-  let users = [];  
-  for (var i = 0; i < 100; i++) {
-    var firstName = faker.name.firstName();
-    var lastName = faker.name.lastName();
-    var phoneNumber = faker.phone.phoneNumberFormat();
-    var image = faker.image.avatar();
-    var email = faker.internet.email();
-    var zipCode = faker.address.zipCode();
-    var streetAddress = faker.address.streetAddress();
-    var city = faker.address.city();
-    var state = faker.address.state();
+var Bakers = [];
 
-    users.push({});
-    // console.log(
-    //   "Name: " + firstName,
-    //   lastName,
-    //   "\nPhone Number: " + phoneNumber,
-    //   "\nAvatar: " + image,
-    //   "\nEmail: " + email,
-    //   "\nZip Code: " + zipCode,
-    //   "\nStreet Address: " + streetAddress,
-    //   "\nCity: " + city,
-    //   "\nState: " + state + "\n-------------------------"
-    // );
-  }
+for (var i = 0; i < 50000; i++) {
+  var firstName = faker.name.firstName();
+  var lastName = faker.name.lastName();
+  var phoneNumber = faker.phone.phoneNumberFormat();
+  var image = faker.image.avatar();
+  var email = faker.internet.email();
+  var zipCode = faker.address.zipCode();
+  var streetAddress = faker.address.streetAddress();
+  var city = faker.address.city();
+  var state = faker.address.state();
+
+  Bakers.push([
+    firstName,
+    lastName,
+    phoneNumber,
+    image,
+    email,
+    zipCode,
+    streetAddress,
+    city,
+    state
+  ]);
+  // console.log(
+  //   "Name: " + firstName,
+  //   lastName,
+  //   "\nPhone Number: " + phoneNumber,
+  //   "\nAvatar: " + image,
+  //   "\nEmail: " + email,
+  //   "\nZip Code: " + zipCode,
+  //   "\nStreet Address: " + streetAddress,
+  //   "\nCity: " + city,
+  //   "\nState: " + state + "\n-------------------------"
+  // );
 }
-generateBakers();
+module.exports = Bakers;
